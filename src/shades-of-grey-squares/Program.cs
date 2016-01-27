604a5639f60303f5faf9268d66a9931b91654924
Luke Parker <moongateuk@gmail.com>
2016-01-27 21:46:09 +0000

    Initial project setup.

---
+++ src/shades-of-grey-squares/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shades_of_grey_squares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
